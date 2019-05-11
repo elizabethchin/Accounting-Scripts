083738081709ffaa495bd9c3402842d0bf8de865
Elizabeth Chin <elizabethtchin@gmail.com>
2019-05-11 06:30:10 +0000

    combine dictionaries into one dictionary

--- melons.py
+++ melons.py
@@ -1,47 +1,37 @@
 melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
-
-flesh_color = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-}
-
-weight = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-}
-
-rind_color = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
+    'Honeydew' : {
+        'melon prices' : 0.99, 
+        'melon seedlessness' : True,
+        'flesh color' : None,
+        'weight' : None,
+        'rind color' : None
+    }
+    'Crenshaw' : {
+        'melon prices' : 2.00, 
+        'melon seedlessness' : False,
+        'flesh color' : None,
+        'weight' : None,
+        'rind color' : None
+    }
+    'Crane' : {
+        'melon prices' : 2.50, 
+        'melon seedlessness' : False,
+        'flesh color' : None,
+        'weight' : None,
+        'rind color' : None
+    }
+    'Casaba' : {
+        'melon prices' : 2.50, 
+        'melon seedlessness' : False,
+        'flesh color' : None,
+        'weight' : None,
+        'rind color' : None
+    }
+    'Cantaloupe' :{
+        'melon prices' : 2.50, 
+        'melon seedlessness' : False,
+        'flesh color' : None,
+        'weight' : None,
+        'rind color' : None
+    }
 }
